7235b8a246b0eea7c08aeb015106641b608f6024
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-03-14 00:29:43 +0100

    add class Food

---
+++ src/Food.java
@@ -0,0 +1,32 @@
+public class Food {
+  private String title;
+  private double calories;
+
+  public Food(String title, double calories) {
+    this.title = title;
+    this.calories = calories;
+  }
+
+  public Food(String title) {
+    this.title = title;
+  }
+
+  public void setCalories(double calories) {
+    this.calories = calories;
+  }
+
+  public double getCalories() {
+    return calories;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void eatFood(){
+    System.out.printf("You just ate %s%n",title);
+    if (calories!=0){
+      System.out.printf("With a calorie count of %.2f calories%n",calories);
+    }
+  }
+}
