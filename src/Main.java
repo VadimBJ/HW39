97b7e4db74bf89eea790fb1d69fb7e88f225662e
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-03-14 01:02:20 +0100

    add class Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.io.IOException;
+
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+  public static void main(String[] args) throws IOException {
+    Food bred = new Food("Bred", 10);
+    bred.eatFood();
+
+    Drink vodka = new Drink("Vodka", 15, false, true);
+    Drink pepsi = new Drink("Pepsi", 30.3, true, false);
+    Drink beer = new Drink("Beer", 20.6, true, true);
+
+    vodka.openDrink();
+    pepsi.openDrink();
+    beer.openDrink();
+
   }
 }
